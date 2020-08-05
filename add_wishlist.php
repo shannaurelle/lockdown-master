defcdd7d10254ad5b90df4307ba20a7b457cf989
shannaurelle <shannaurelleg@gmail.com>
2020-08-05 11:05:52 +0800

    Add wishlist update 2

--- add_wishlist.php
+++ add_wishlist.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect('localhost',"root","","lockdown-storage");
+$con = mysqli_connect('localhost','root','','lockdown-storage');
 if(mysqli_connect_errno()){
     printf("Connect error: %s \n",mysqli_connect_error);
 }
@@ -10,9 +10,9 @@ $account_id = filter_var($_POST['account_id'],FILTER_SANITIZE_NUMBER_INT);
 $stmt = mysqli_stmt_init($con);
 
 if(mysqli_stmt_prepare($stmt,"INSERT FROM wishlist (product_id, account_id) VALUES (?,?)")){
-    mysqli_stmt_bind_param($stmt,"ii",$product_id,$account_id);
+    mysqli_stmt_bind_param($stmt,'ii',$product_id,$account_id);
     mysqli_stmt_execute($stmt);
-    header("Location: wishlist.html");
+    header('Location: wishlist.html');
 }
 
 exit();
