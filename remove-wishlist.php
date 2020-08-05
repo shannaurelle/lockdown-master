393d7d5c77b52336bc01a3dc9e250fe606f07250
shannaurelle <shannaurelleg@gmail.com>
2020-08-05 11:05:02 +0800

    Remove wishlist update 2

--- remove-wishlist.php
+++ remove-wishlist.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect('localhost',"root","","lockdown-storage");
+$con = mysqli_connect('localhost','root','','lockdown-storage');
 if(mysqli_connect_errno()){
     printf("Connect error: %s \n",mysqli_connect_error);
 }
@@ -9,11 +9,11 @@ $wishlist_id = filter_var($_POST['wishlist_id'],FILTER_SANITIZE_NUMBER_INT);
 $stmt = mysqli_stmt_init($con);
 
 if(mysqli_stmt_prepare($stmt,"DELETE FROM wishlist WHERE id=?")){
-    mysqli_stmt_bind_param($stmt,"i",$wishlist_id);
+    mysqli_stmt_bind_param($stmt,'i',$wishlist_id);
     mysqli_stmt_execute($stmt);
-    header("Location: wishlist.html");
+    header('Location: wishlist.html');
 }
-header("Location: error/404.html");
+header('Location: error/404.html');
 exit();
 
 ?>
