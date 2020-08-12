454938afe3b008b92e4915089e771132c167e85c
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2020-08-12 17:36:44 +0800

    Update shop.php

--- shop.php
+++ shop.php
@@ -364,29 +364,56 @@ $con=mysqli_connect("localhost", "root", "");
                 <div class="tab-pane active" id="all">
                     <ul class="row">
                     <?php
-                        $sql = "SELECT * FROM `products`";
-                        if (isset($_GET['search'])){
-                            $result = mysqli_query($con,"SELECT * FROM `products` WHERE MATCH(`product_name`) AGAINST('" . $_GET['search'] . "' IN NATURAL LANGUAGE MODE) ORDER BY product_date DESC");
-                        }
-                        if (isset($_GET['sortbyfilter'])) {
-                            if ($_GET['sortbyfilter'] == "new") {
-                                $sql .= " ORDER BY product_date DESC";
+                        if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+                            $page_no = $_GET['page_no'];
+                            } else {
+                                $page_no = 1;
+                                }
+
+                            $total_records_per_page = 10;
+                            $offset = ($page_no-1) * $total_records_per_page;
+                            $previous_page = $page_no - 1;
+                            $next_page = $page_no + 1;
+                            $adjacents = "2"; 
+
+                            
+                            if (isset($_GET['search'])){
+                                $result = mysqli_query($con,"SELECT COUNT(*) As total_records FROM `products` WHERE MATCH(`product_name`) AGAINST('" . $_GET['search'] . "' IN NATURAL LANGUAGE MODE) ORDER BY product_date DESC");
                             }
-                            elseif ($_GET['sortbyfilter'] == "pricehigh") {
-                                $sql .= " ORDER BY product_price DESC";
+                            else {
+                                $result_count = mysqli_query($con,"SELECT COUNT(*) As total_records FROM `products`");
                             }
-                            elseif ($_GET['sortbyfilter'] == "pricelow") {
-                                $sql .= " ORDER BY product_price ASC";
+                            
+
+                            $total_records = mysqli_fetch_array($result_count);
+                            $total_records = $total_records['total_records'];
+                            $total_no_of_pages = ceil($total_records / $total_records_per_page);
+                            $second_last = $total_no_of_pages - 1; // total page minus 1
+
+                            $sql = "SELECT * FROM `products`";
+                            if (isset($_GET['search'])){
+                                $result_1 = mysqli_query($con,"SELECT * FROM `products` WHERE MATCH(`product_name`) AGAINST('" . $_GET['search'] . "' IN NATURAL LANGUAGE MODE) ORDER BY product_date DESC");
                             }
-                            elseif ($_GET['sortbyfilter'] == "volumehigh") {
-                                $sql .= " ORDER BY product_volume DESC";
+                            if (isset($_GET['sortbyfilter'])) {
+                                if ($_GET['sortbyfilter'] == "new") {
+                                    $sql .= " ORDER BY product_date DESC";
+                                }
+                                elseif ($_GET['sortbyfilter'] == "pricehigh") {
+                                    $sql .= " ORDER BY product_price DESC";
+                                }
+                                elseif ($_GET['sortbyfilter'] == "pricelow") {
+                                    $sql .= " ORDER BY product_price ASC";
+                                }
+                                elseif ($_GET['sortbyfilter'] == "volumehigh") {
+                                    $sql .= " ORDER BY product_volume DESC";
+                                }
+                                elseif ($_GET['sortbyfilter'] == "volumelow") {
+                                    $sql .= " ORDER BY product_volume ASC";
+                                }
                             }
-                            elseif ($_GET['sortbyfilter'] == "volumelow") {
-                                $sql .= " ORDER BY product_volume ASC";
-                            }
-                        }
-                        $result = mysqli_query($con,$sql);
-                        while($row = mysqli_fetch_array($result)){
+                            $sql .= " LIMIT $offset, $total_records_per_page";
+                            $result_1 = mysqli_query($con,$sql);
+                        while($row = mysqli_fetch_array($result_1)){
                             echo "<li class='col-lg-3 col-sm-6 col-12'>";
                             echo "<div class='product-wrap'>";
                             echo "<div class='product-img'>";
@@ -418,9 +445,86 @@ $con=mysqli_connect("localhost", "root", "");
                             echo "</li>";
 
                         }
-
-
+                            mysqli_close($con);
                     ?>
+                    <ul>
+                        <nav aria-label="Page navigation example">
+                            <div class='text-center' style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+                                <strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+                            </div>
+                            <ul class="pagination justify-content-center">
+                                <?php if($page_no > 1){ echo "<li class='page-item'><a class='page-link' href='?page_no=1'>&lsaquo;&lsaquo; First </a></li>"; } ?>
+                                
+                                <li <?php if($page_no <= 1){ echo "class='disabled page-item'"; } ?>>
+                                    <a class="page-link"<?php if($page_no > 1){ echo "href='?page_no=$previous_page'"; } ?>>Previous</a>
+                                </li>
+                                   
+                                <?php 
+                                if ($total_no_of_pages <= 10){       
+                                    for ($counter = 1; $counter <= $total_no_of_pages; $counter++){
+                                        if ($counter == $page_no) {
+                                       echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";  
+                                            }else{
+                                       echo "<li class='page-item'><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
+                                            }
+                                    }
+                                }
+                                elseif($total_no_of_pages > 10){
+                                    
+                                if($page_no <= 4) {         
+                                 for ($counter = 1; $counter < 8; $counter++){       
+                                        if ($counter == $page_no) {
+                                       echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";  
+                                            }else{
+                                       echo "<li class='page-item'><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
+                                            }
+                                    }
+                                    echo "<li class='page-item'><a class='page-link>...</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=$second_last'>$second_last</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+                                    }
+
+                                 elseif($page_no > 4 && $page_no < $total_no_of_pages - 4) {         
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=1'>1</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=2'>2</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' >...</a></li>";
+                                    for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {         
+                                       if ($counter == $page_no) {
+                                       echo "<li class='page-item active'><a>$counter</a></li>";    
+                                            }else{
+                                       echo "<li class='page-item'><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
+                                            }                  
+                                   }
+                                   echo "<li class='page-item'><a class='page-link'>...</a></li>";
+                                   echo "<li class='page-item'><a  class='page-link' href='?page_no=$second_last'>$second_last</a></li>";
+                                   echo "<li class='page-item'><a  class='page-link' href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";      
+                                        }
+                                    
+                                    else {
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=1'>1</a></li>";
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=2'>2</a></li>";
+                                    echo "<li class='page-item'><a class='page-link'>...</a></li>";
+
+                                    for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
+                                      if ($counter == $page_no) {
+                                       echo "<li class='page-item active'><a class='page-link'>$counter</a></li>";  
+                                            }else{
+                                       echo "<li class='page-item'><a class='page-link' href='?page_no=$counter'>$counter</a></li>";
+                                            }                   
+                                            }
+                                        }
+                                }
+                                ?>
+                                
+                                <li class="page-item" <?php if($page_no >= $total_no_of_pages){ echo "class='disabled'"; } ?>>
+                                <a class="page-link" <?php if($page_no < $total_no_of_pages) { echo "href='?page_no=$next_page'"; } ?>>Next</a>
+                                </li>
+                                <?php if($page_no < $total_no_of_pages){
+                                    echo "<li class='page-item'><a class='page-link' href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+                                    } ?>
+                            </ul>
+                        </nav>
+                    </ul>
                 </div>
             </div>
         </div>
