23f20a89b18345d5286170253dd634b7886e9416
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2020-08-14 09:12:19 +0800

    shop.php additions
    sql changed in order to account for the 'cart' table. add to cart functionalities added. add_cart php refined for fitted use

--- add_cart.php
+++ add_cart.php
@@ -4,12 +4,14 @@ $connection = mysqli_connect('localhost','root','','lockdown-storage');
 if(mysqli_connect_errno()){
     printf("Connect error: %s \n",mysqli_connect_error());
 }
-$account_id = filter_var($_POST['account_id'],FILTER_SANITIZE_NUMBER_INT);
+$account_id = filter_var($_SESSION['account_id'],FILTER_SANITIZE_NUMBER_INT);
 $product_id = filter_var($_POST['product_id'],FILTER_SANITIZE_NUMBER_INT);
-$cart_owner = filter_var($_POST['cart_owner'],FILTER_VALIDATE_STRING);
+$product_volume = filter_var($_POST['product_volume'],FILTER_SANITIZE_NUMBER_INT);
+$cart_owner = filter_var($_SESSION['cart_owner'],FILTER_VALIDATE_STRING);
+
 $stmt = mysqli_stmt_init($connection);
 
-$sql = "INSERT INTO cart_items (product_id, account_id, cart_owner) VALUES (?,?,?)";
+$sql = "INSERT INTO cart_items (product_id, account_id, product_volume, cart_owner,) VALUES (?,?,?,?)";
 
 if(mysqli_stmt_prepare($stmt,$sql)){
     mysqli_stmt_bind_param($stmt,'i',$product_id);
