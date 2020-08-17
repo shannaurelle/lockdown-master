5311816794f9c0cc2e2083f29b922b341c289d5b
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2020-08-17 09:21:47 +0800

    Update previous_transactions.php

--- previous_transactions.php
+++ previous_transactions.php
@@ -386,6 +386,36 @@
             </form>
             <div class="tab-content">
                 <div class="tab-pane active" id="all">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">First</th>
+                          <th scope="col">Last</th>
+                          <th scope="col">Handle</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td>Larry</td>
+                          <td>the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>
                     <ul class="row">
                     <?php
                         if (isset($_GET['page_no']) && $_GET['page_no']!="") {
