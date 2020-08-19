a5935162c506df035b119001326f873fd70b5c1a
shannaurelle <shannaurelleg@gmail.com>
2020-08-19 17:11:03 +0800

    ignore error pls

--- create_account.php
+++ create_account.php
@@ -8,26 +8,25 @@ if(mysqli_connect_errno()){
 // Security purposes
 if($_SERVER['REQUEST_METHOD'] == "POST"){
 
-// text fields 
-$first_name = filter_var($_POST['first_name'],FILTER_VALIDATE_STRING) ?? '';
-$middle_name = filter_var($_POST['middle_name'],FILTER_VALIDATE_STRING) ?? '';
-$last_name = filter_var($_POST['last_name'],FILTER_VALIDATE_STRING) ?? '';
+$first_name = filter_var($_POST['first_name'],FILTER_SANITIZE_STRING) ?? '';
+$middle_name = filter_var($_POST['middle_name'],FILTER_SANITIZE_STRING) ?? '';
+$last_name = filter_var($_POST['last_name'],FILTER_SANITIZE_STRING) ?? '';
 $email = filter_var($_POST['email'],FILTER_VALIDATE_EMAIL) ?? '';
-$phone_number = filter_var($_POST['phone_number'],FILTER_VALIDATE_STRING) ?? '';
-$address = filter_var($_POST['address'],FILTER_VALIDATE_STRING) ?? '';
-$province = filter_var($_POST['province'],FILTER_VALIDATE_STRING) ?? '';
-$city = filter_var($_POST['city'],FILTER_VALIDATE_STRING) ?? '';
-$country = "PH";
-$field_of_interest = filter_var($_POST['field_of_interest'],FILTER_VALIDATE_STRING) ?? '';
-$paymaya_account_number = filter_var($_POST['paymaya_account_number'],FILTER_VALIDATE_STRING) ?? '';
-$TIN_number = filter_var($_POST['TIN_number'],FILTER_VALIDATE_STRING) ?? '';
+$phone_number = filter_var($_POST['phone_number'],FILTER_SANITIZE_STRING) ?? '';
+$address = filter_var($_POST['address'],FILTER_SANITIZE_STRING) ?? '';
+$province = filter_var($_POST['province'],FILTER_SANITIZE_STRING) ?? '';
+$city = filter_var($_POST['city'],FILTER_SANITIZE_STRING) ?? '';
+$country = 'PH';
+$field_of_interest = filter_var($_POST['field_of_interest'],FILTER_SANITIZE_STRING) ?? '';
+$paymaya_account_number = filter_var($_POST['paymaya_account_number'],FILTER_SANITIZE_STRING) ?? '';
+$TIN_number = filter_var($_POST['TIN_number'],FILTER_SANITIZE_STRING) ?? '';
+$date_created = date('Y-m-d H:i:s');
 
-$access = filter_var($_POST['access'],FILTER_VALIDATE_STRING) ?? '';
-$username = filter_var($_POST['username'],FILTER_VALIDATE_STRING) ?? '';
-$password = filter_var($_POST['password'],FILTER_VALIDATE_STRING) ?? '';
+$access = filter_var($_POST['access'],FILTER_SANITIZE_STRING) ?? '';
+$username = filter_var($_POST['username'],FILTER_SANITIZE_STRING) ?? '';
+$password = filter_var($_POST['password'],FILTER_SANITIZE_STRING) ?? '';
 $password = password_hash($password,PASSWORD_DEFAULT) ?? '';
 
-// make new directories if there's no existing one
 if(!file_exists("assets")){mkdir("upload"); }
 if(!file_exists("assets/images")){mkdir("upload/images"); } 
 if(!file_exists("assets/images/business_permit")){mkdir("upload/images/business_permit"); } 
@@ -35,60 +34,39 @@ if(!file_exists("assets/images/government_ID")){mkdir("upload/images/government_
 if(!file_exists("assets/images/selfie")){mkdir("upload/images/selfie"); } 
 if(!file_exists("assets/images/certifications")){mkdir("upload/images/certifications"); } 
 
-//Сheck that we have a business permit file (preferrably in picture)
-if(is_uploaded_file($_FILES['business_permit']['tmp-name'])) {
+if(isset($_FILES['business_permit']['tmp-name'])) { $tmpFilePath = $_FILES['business_permit']['tmp-name']; }
 
- //Check if the file is rar folder 
-  $business_permit_filename = basename($_FILES['business_permit']['name']);
-    //Determine the path to which we want to save this file
-      $business_permit_newname = dirname(__FILE__).'/'.$filename;
- //Check if the file with the same name is already exists on the server
+if(isset($tmpFilePath) && is_uploaded_file($tmpFilePath)) {
+
+  $business_permit_filename = $_FILES['business_permit']['name'];
+
+  $business_permit_newname = $_SERVER['DOCUMENT_ROOT'].'/assets/images/business-permit/'.$filename;
+
  if (!file_exists($business_permit_newname)) {
-//-------------------------
      
-     move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$business_permit_newname);
+     move_uploaded_file($_FILES['business_permit']['tmp_name'],$business_permit_newname);
     }
-    else {  // display popup when account already exists from the upload directory
+    else {  
         echo "<script type='text/javascript'>
         alert('Account already exists!');
         document.location.href = 'index.html';
         </script>";
         }
 }
-//Сheck that we have a business permit file (preferrably in picture)
-if(is_uploaded_file($_FILES['business_permit']['tmp-name'])) {
 
-    //Check if the file is rar folder 
-     $business_permit_filename = basename($_FILES['business_permit']['name']);
-       //Determine the path to which we want to save this file
-         $business_permit_newname = dirname(__FILE__).'/assets/images/business_permit/'.$business_permit_filename;
-    //Check if the file with the same name is already exists on the server
-    if (!file_exists($business_permit_newname)) {
-   //-------------------------
-        
-        move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$business_permit_newname);
-       }
-       else {  // display popup when account already exists from the upload directory
-           echo "<script type='text/javascript'>
-           alert('File already exists!');
-           document.location.href = 'index.html';
-           </script>";
-           }
-   }
-//Сheck that we have a government ID file (preferrably in picture)
-if(is_uploaded_file($_FILES['government_ID_picture']['tmp-name'])) {
+if(isset($_FILES['government_ID_picture']['tmp-name'])) { $tmpFilePath = $_FILES['government_ID_picture']['tmp-name']; }
 
-    //Check if the file is rar folder 
+if(isset($tmpFilePath) && is_uploaded_file($tmpFilePath)) {
+
      $government_ID_picture_filename = basename($_FILES['government_ID_picture']['name']);
-       //Determine the path to which we want to save this file
-         $government_ID_picture_newname = dirname(__FILE__).'/'.$government_ID_picture_filename;
-    //Check if the file with the same name is already exists on the server
+     
+         $government_ID_picture_newname = "./assets/images/government_ID/".$government_ID_picture_filename;
+    
     if (!file_exists($government_ID_picture_newname)) {
-   //-------------------------
         
-        move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$government_ID_picture_newname);
+        move_uploaded_file($_FILES['government_ID_picture']['tmp_name'],$government_ID_picture_newname);
        }
-       else {  // display popup when account already exists from the upload directory
+       else {  
            echo "<script type='text/javascript'>
            alert('File already exists!');
            document.location.href = 'index.html';
@@ -96,20 +74,19 @@ if(is_uploaded_file($_FILES['government_ID_picture']['tmp-name'])) {
            }
    }
 
-if(is_uploaded_file($_FILES['selfie_with_government_ID']['tmp-name'])) {
+if(isset($_FILES['selfie_with_government_ID']['tmp-name'])) { $tmpFilePath = $_FILES['selfie_with_government_ID']['tmp-name']; }
 
-//Check if the file is rar folder 
+if(isset($tmpFilePath) && is_uploaded_file($_FILES['selfie_with_government_ID']['tmp-name'])) {
+
     $selfie_with_government_ID_filename = basename($_FILES['selfie_with_government_ID']['name']);
-    //Determine the path to which we want to save this file
-        $selfie_with_government_ID_newname = dirname(__FILE__).'/'.$selfie_with_government_ID_filename;
-
-//Check if the file with the same name is already exists on the server
-if (!file_exists($selfie_with_government_ID_newname)) {
-//-------------------------
     
-    move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$selfie_with_government_ID_newname);
+        $selfie_with_government_ID_newname = "./assets/images/selfie/" .$selfie_with_government_ID_filename;
+
+if (!file_exists($selfie_with_government_ID_newname)) {
+    
+    move_uploaded_file($_FILES['selfie_with_government_ID']['tmp_name'],$selfie_with_government_ID_newname);
     }
-    else {  // display popup when account already exists from the upload directory
+    else {  
         echo "<script type='text/javascript'>
         alert('File already exists!');
         document.location.href = 'index.html';
@@ -117,10 +94,10 @@ if (!file_exists($selfie_with_government_ID_newname)) {
         }
 }
 
-$total = count($_FILES['certifications']['name']);
+define("TOTAL_CERTIFICATIONS", count($_FILES['certifications']['name']));
 
 // Loop through each file
-for( $i=0 ; $i < $total ; $i++ ) {
+for( $i=0 ; $i < TOTAL_CERTIFICATIONS ; $i++ ) {
 
   //Get the temp file path
   $tmpFilePath = $_FILES['certifications']['tmp_name'][$i];
@@ -132,7 +109,7 @@ for( $i=0 ; $i < $total ; $i++ ) {
 
     //Upload the file into the temp dir
     if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-
+        
     }
   }
 }
@@ -141,11 +118,11 @@ $query_stmt = mysqli_stmt_init($connection);
 
 $query_stmt_2 = mysqli_stmt_init($connection);
 
-$sql = 'INSERT INTO accounts_info (field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
+$sql = 'INSERT INTO account_info (field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
 
-$sql2 = 'INSERT INTO accounts (username, password, access) VALUES (?,?,?)';
+$sql2 = 'INSERT INTO accounts (username, password, access, date_created) VALUES (?,?,?,?)';
 
-if(mysqli_stmt_prepare($query_stmt, $sql) && mysqli_stmt_prepare($query_stmt, $sql2)){
+if(mysqli_stmt_prepare($query_stmt, $sql) && mysqli_stmt_prepare($query_stmt_2, $sql2)){
     
     if(!mysqli_stmt_bind_param($query_stmt,'sssssssssssss',$first_name,$middle_name,$last_name,$email,$phone_number,$address,$province, $city, $country, $field_of_interest,$paymaya_account_number,$TIN_number, $username)){
         echo "Binding parameters failed: ".mysqli_stmt_error($query_stmt); exit;
@@ -155,7 +132,7 @@ if(mysqli_stmt_prepare($query_stmt, $sql) && mysqli_stmt_prepare($query_stmt, $s
         echo "Statment execution failed: ".mysqli_stmt_error($query_stmt); exit;
     }
 
-    if(!mysqli_stmt_bind_param($query_stmt_2,'sss',$username,$password,$access)){
+    if(!mysqli_stmt_bind_param($query_stmt_2,'ssss',$username,$password,$access, $date_created)){
         echo "Binding parameters failed: ".mysqli_stmt_error($query_stmt); exit;
     }
 
@@ -165,23 +142,23 @@ if(mysqli_stmt_prepare($query_stmt, $sql) && mysqli_stmt_prepare($query_stmt, $s
     
     echo "<script type='text/javascript'> 
         alert('A new document is added successfully');
-        document.location.href = 'register.php';
+        document.location.href = 'index.html';
         </script>";
     }
 
     else{
         echo "<script type='text/javascript'> 
         alert('Preparation failed!');
-        document.location.href = 'register.php';
         </script>";
     }
       
-mysqli_stmt_close($query_stmt);
+    if(!mysqli_stmt_close($query_stmt)) { echo "Statment execution failed: ".mysqli_stmt_error($query_stmt); }
+    if(!mysqli_stmt_close($query_stmt_2)) { echo "Statment execution failed: ".mysqli_stmt_error($query_stmt); }
 		
 }
 else{
     header("HTTP/1.1 403 Origin Denied");
 }
 // close the mysql connection after use
-mysqli_close($con);  
+mysqli_close($connection);  
 exit;   
