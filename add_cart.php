a41575777c8f6bccf2f72701bd61a59434d25622
shannaurelle <shannaurelleg@gmail.com>
2020-08-20 11:11:36 +0800

    edits and more edits

--- add_cart.php
+++ add_cart.php
@@ -4,17 +4,18 @@ $connection = mysqli_connect('localhost','root','','lockdown-storage');
 if(mysqli_connect_errno()){
     printf("Connect error: %s \n",mysqli_connect_error());
 }
+session_start();
 $account_id = filter_var($_SESSION['account_id'],FILTER_SANITIZE_NUMBER_INT);
 $product_id = filter_var($_POST['product_id'],FILTER_SANITIZE_NUMBER_INT);
 $product_volume = filter_var($_POST['product_volume'],FILTER_SANITIZE_NUMBER_INT);
-$cart_owner = filter_var($_SESSION['cart_owner'],FILTER_VALIDATE_STRING);
+if(isset($cart_owner)){ $cart_owner = filter_var($_SESSION['cart_owner'],FILTER_SANITIZE_STRING); } 
 
 $stmt = mysqli_stmt_init($connection);
 
-$sql = "INSERT INTO cart_items (product_id, account_id, product_volume, cart_owner,) VALUES (?,?,?,?)";
+$sql = "INSERT INTO cart_items (product_id, account_id, product_volume) VALUES (?,?,?)";
 
 if(mysqli_stmt_prepare($stmt,$sql)){
-    mysqli_stmt_bind_param($stmt,'i',$product_id);
+    mysqli_stmt_bind_param($stmt,'iss',$product_id, $account_id, $product_volume);
     mysqli_stmt_execute($stmt);
     header('Location: cart.html');
 }
