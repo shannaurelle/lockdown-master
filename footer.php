e95c9177aba7a9d381f2c0a6b20cb502c484264c
shannaurelle <shannaurelleg@gmail.com>
2020-08-21 18:51:59 +0800

    universal footer page

---
+++ footer.php
@@ -0,0 +1,74 @@
+<!-- product-area end -->
+<footer class="footer-area">
+        <div class="footer-top bg-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6 col-12">
+                        <div class="footer-widget footer-logo">
+                            <img src="assets/images/logo2.png" alt="">
+                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from</p>
+                            <ul class="socil-icon d-flex">
+                                <li><a href="javascript:void(0);"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="javascript:void(0);"><i class="fa fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-12">
+                        <div class="footer-widget footer-menu">
+                            <h2>Spacial Menu</h2>
+                            <ul>
+                                <li><a href="account.html">My Account</a></li>
+                                <li><a href="checkout.html">Checkout</a></li>
+                                <li><a href="javascript:void(0)">Help</a></li>
+                                <li><a href="javascript:void(0)">Support</a></li>
+                                <li><a href="javascript:void(0)">FAQ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-12">
+                        <div class="footer-widget footer-contact">
+                            <h2>Contact us</h2>
+                            <ul>
+                                <li><i class="fa fa-map-marker"></i>House No. 09 , Road No.25 Dhaka,Bangladesh </li>
+                                <li><i class="fa fa-phone"></i>+1(888)234-56789 <span>+1(888)234-56789</span> </li>
+                                <li><i class="fa fa-envelope-o"></i>youremail@gmail.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6 col-12">
+                        <div class="footer-widget footer-contact">
+                            <h2>Join to Newsletter</h2>
+                            <p>Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from</p>
+                            <form class="sform">
+                                <div class="form_msg">
+                                    <label class="mt10"></label>
+                                </div>
+                                <input type="email" name="email" placeholder="Enter Your Email" required>
+                                <button type="submit" name="submit" id="subscribe-btn"><i class="fa fa-long-arrow-right"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="footer-buttom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-12">
+                        <p>&copy;2018 Your Website Name All Right Reserved</p>
+                    </div>
+                    <div class="col-md-6 col-12">
+                        <ul class="d-flex">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="blog.html">Blog</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
