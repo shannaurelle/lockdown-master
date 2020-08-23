f59b5e8f0e173918da4f7dbe29a380c1e1f08a01
shannaurelle <shannaurelleg@gmail.com>
2020-08-24 00:39:24 +0800

    edited

--- accept_request.php
+++ accept_request.php
@@ -8,13 +8,20 @@ session_start();
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $transaction_id = filter_var($_POST['transaction_id'],FILTER_SANITIZE_NUMBER_INT);
     $stmt = mysqli_stmt_init($connection);
+    $stmt2 = mysqli_stmt_init($connection);
     $sql = "INSERT INTO trades (transaction_id, buyer_id, trucker_id, product_id, money, notes, pending) ";
     $sql .= "SELECT (transaction_id, buyer_id, owner_id, product_id, product_volume, money, notes, pending) FROM request";
     $sql .= "WHERE transaction_id = ? ";
-    if(mysqli_stmt_prepare($stmt,$sql)){
+    $sql2 = "UPDATE request SET pending='0' WHERE transaction_id = ?";
+    if(mysqli_stmt_prepare($stmt,$sql) && mysqli_stmt_prepare($stmt2,$sql2)){
         mysqli_stmt_bind_param($stmt,"i",$transaction_id);
+        mysqli_stmt_bind_param($stmt2,"i",$transaction_id);
         mysqli_stmt_execute($stmt);
+        mysqli_stmt_execute($stmt2);
+        mysqli_stmt_close($stmt);
+        mysqli_stmt_close($stmt2);
     }
 }
-
+mysqli_close($connection);
+exit;
 ?>
