95075f786393e5c7895f233ee1ccc3ab41e894fc
shannaurelle <shannaurelleg@gmail.com>
2020-08-24 01:57:22 +0800

    edit

--- add_cart.php
+++ add_cart.php
@@ -8,11 +8,11 @@ session_start();
 $account_id = filter_var($_POST['account_id'],FILTER_SANITIZE_NUMBER_INT);
 $product_id = filter_var($_POST['product_id'],FILTER_SANITIZE_NUMBER_INT);
 $product_volume = filter_var($_POST['product_volume'],FILTER_SANITIZE_NUMBER_INT);
-if(isset($cart_owner)){ $cart_owner = filter_var($_SESSION['cart_owner'],FILTER_SANITIZE_STRING); } 
+$product_price = filter_var($_POST['product_price'],FILTER_SANITIZE_NUMBER_INT);
 
 $stmt = mysqli_stmt_init($connection);
 
-$sql = "INSERT INTO cart (cart_id, product_id, product_volume) VALUES (?,?,?)";
+$sql = "INSERT INTO cart (cart_id, product_id, product_volume, product_price) VALUES (?,?,?,?)";
 
 if(mysqli_stmt_prepare($stmt,$sql)){
     mysqli_stmt_bind_param($stmt,'iis',$account_id, $product_id, $product_volume);
