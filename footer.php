80b33e7948e47c7b47d41d7f99be7ca3c0a04fc4
shannaurelle <shannaurelleg@gmail.com>
2020-08-23 23:09:07 +0800

    edited footer

--- footer.php
+++ footer.php
@@ -5,13 +5,14 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 col-12">
-                        <p class="text-dark" >&copy;2018 Your Website Name All Right Reserved</p>
+                        <p class="text-dark" >&copy;2020 The Online Marketplace All Right Reserved</p>
                     </div>
                     <div class="col-md-6 col-12">
                         <ul class="d-flex">
                             <li><a class="text-dark" href="index.html">Home</a></li>
-                            <li><a class="text-dark" href="blog.html">Blog</a></li>
-                            <li><a class="text-dark" href="contact.html">Contact Us</a></li>
+                            <?php if(isset($_SESSION['access'])): ?>
+                            <li><a class="text-dark" href="logout.php">Logout</a>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
